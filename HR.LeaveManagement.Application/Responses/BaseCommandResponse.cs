27c884044be033c8c317abf1afead5914c79b047
omrs2002 <omrs2002@yahoo.com>
2021-10-12 11:29:23 +0300

    ADD CQRS

---
+++ HR.LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public int Id { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+
+}
