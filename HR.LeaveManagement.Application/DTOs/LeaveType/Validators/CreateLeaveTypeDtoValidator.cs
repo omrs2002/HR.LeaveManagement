0c6b72dd9dbade287d2e1b7efb5befb8312e96c4
omrs2002 <omrs2002@yahoo.com>
2021-10-12 15:55:19 +0300

    Add Validations

---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validators
+{
+    class CreateLeaveTypeDtoValidator : AbstractValidator<CreateLeaveTypeDto>
+    {
+        public CreateLeaveTypeDtoValidator()
+        {
+
+            Include(new ILeaveTypeDtoValidator());
+            
+            //RuleFor(p => p.Name)
+            //   .NotEmpty().WithMessage("{PropertyName} is required.")
+            //   .NotNull()
+            //   .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
+
+            //RuleFor(p => p.DefaultDays)
+            //    .NotEmpty().WithMessage("{PropertyName} is required.")
+            //    .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+            //    .LessThan(100).WithMessage("{PropertyName} must be less than {ComparisonValue}.");
+        }
+    }
+}
--- HR.LeaveManagement.Application/Features/LeaveType/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using HR.LeaveManagement.Application.Features.LeaveType.Requests.Commands;
+using HR.LeaveManagement.Application.DTOs.LeaveType.Validators;
 using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
 using HR.LeaveManagement.Application.Persistence.Contracts;
 using HR.LeaveManagement.Application.Responses;
@@ -27,17 +27,18 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Handlers.Commands
         public async Task<BaseCommandResponse> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
             var response = new BaseCommandResponse();
-            //var validator = new CreateLeaveTypeDtoValidator();
-            //var validationResult = await validator.ValidateAsync(request.LeaveTypeDto);
+            var validator = new CreateLeaveTypeDtoValidator();
+            var validationResult = await validator.ValidateAsync(request.LeaveTypeDto);
 
-            //if (validationResult.IsValid == false)
-            //{
-            //    response.Success = false;
-            //    response.Message = "Creation Failed";
-            //    response.Errors = validationResult.Errors.Select(q => q.ErrorMessage).ToList();
-            //}
-            //else
-            //{
+            if (validationResult.IsValid == false)
+            {
+                response.Success = false;
+                response.Message = "creation failed";
+                response.Errors = validationResult.Errors.Select(q => q.ErrorMessage).ToList();
+
+            }
+            else
+            {
                 var leaveType = _mapper.Map<Domain.LeaveType>(request.LeaveTypeDto);
 
                 leaveType = await _unitOfWork.LeaveTypeRepository.Add(leaveType);
@@ -46,7 +47,7 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Handlers.Commands
                 response.Success = true;
                 response.Message = "Creation Successful";
                 response.Id = leaveType.Id;
-            //}
+            }
 
             return response;
         }
