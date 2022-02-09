edb49160be79df60e894a2ffe3e596f93771cd9a
omrs2002 <omrs2002@yahoo.com>
2022-02-09 23:48:39 +0300

    Add missing services to web

--- HR.LeaveManagement.MVC/Models/ErrorViewModel.cs
+++ HR.LeaveManagement.MVC/Models/ErrorViewModel.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace HR.LeaveManagement.MVC.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
