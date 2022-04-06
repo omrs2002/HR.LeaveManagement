910c781a42511c70b4f0b8e3921824c41060185e
omrs2002 <omrs2002@yahoo.com>
2022-04-07 00:37:59 +0300

    Add [Authorize] to LeaveTypesController

--- HR.LeaveManagement.Api/Controllers/LeaveTypesController.cs
+++ HR.LeaveManagement.Api/Controllers/LeaveTypesController.cs
@@ -2,6 +2,7 @@
 using HR.LeaveManagement.Application.Features.LeaveType.Requests.Queries;
 using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +11,7 @@ namespace HR.LeaveManagement.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class LeaveTypesController : ControllerBase
     {
         private readonly IMediator _mediator;
